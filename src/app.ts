995f04fb45656acaf11f6ac381fc9451978257aa
Simon <33012273+5E7EN@users.noreply.github.com>
2023-02-10 02:11:47 -0500

    Implements Flight Manager

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import chalk from 'chalk';
 import fs from 'fs';
-import { createCursor, getRandomPagePoint, installMouseHelper } from 'ghost-cursor';
 import twilio, { Twilio } from 'twilio';
+
+import { FlightManager } from './helpers/flight-manager';
 
 import { WinstonLogger, buildConfigWithDefaults, addDaysToDate } from './utils';
 import { ChromeInstance } from './helpers';
 import type { BaseLogger } from './utils';
-import type { IFlightResponse, IFlight } from './types/flights';
+import type { IFlight } from './types/flights';
 
 // Configure environmental variables
 dotenv.config();
@@ -51,6 +52,7 @@ interface IUpgradableFlight {
 
 export class App {
     private readonly _chromeInstance: ChromeInstance;
+    private readonly _flightManager: FlightManager;
     private readonly _smsClient: Twilio;
     private readonly _config: IAppConfig;
     private readonly _logger: BaseLogger;
@@ -69,6 +71,11 @@ export class App {
             debug: this._config.debug
         });
 
+        // Create flight manager instance
+        this._flightManager = new FlightManager({
+            chrome: this._chromeInstance
+        });
+
         // Configure SMS notifier
         this._smsClient = twilio(this._config.twilio.authID, this._config.twilio.authToken);
 
@@ -77,148 +84,6 @@ export class App {
 
         // Configure class-wide logger
         this._logger = new WinstonLogger('Main').logger;
-    }
-
-    public async getFlights(origin: string, destination: string, date: string): Promise<IFlight[]> {
-        const flights: IFlight[] = [];
-
-        // Open Chrome
-        this._logger.debug('Launching chrome...');
-        await this._chromeInstance.launch();
-
-        // Launch page
-        this._logger.debug('Opening page...');
-        const page = await this._chromeInstance.instance.newPage();
-        const cursor = createCursor(page, await getRandomPagePoint(page), true);
-        await installMouseHelper(page);
-
-        // Register listener to parse flights API response
-        page.on('response', async (response) => {
-            if (
-                response
-                    .url()
-                    .startsWith(
-                        'https://www.united.com/ual/en/us/flight-search/book-a-flight/flightshopping/getflightresults/rev'
-                    ) &&
-                response.status() === 200
-            ) {
-                // Parse response
-                const data = (await response.json()) as IFlightResponse;
-
-                // Check for API-native errors
-                if (data.status !== 'success') {
-                    this._logger.error(`Encountered errors: ${data.errors.join(', ')}`);
-                }
-
-                // Store remote flights
-                const remoteFlights = data.data.Trips[0]?.Flights;
-
-                // Ensure we got some flight results, and store them
-                if (!remoteFlights || !Array.isArray(remoteFlights)) {
-                    this._logger.warn('No flights found in batch');
-                } else {
-                    flights.push(...remoteFlights);
-                }
-
-                this._logger.debug('Flight batch acquired');
-            }
-        });
-
-        // Clear browser cookies
-        const client = await page.target().createCDPSession();
-        await client.send('Network.clearBrowserCookies');
-        await client.detach();
-
-        // Navigate to page
-        this._logger.debug('Navigating...');
-        await page.goto('https://www.united.com/ual/en/us/flight-search/book-a-flight', {
-            waitUntil: 'networkidle0'
-        });
-
-        // Click one way, and keep trying if cursor missed the button
-        const clickOneWay = async () => {
-            // Click one way
-            this._logger.debug('Clicking one way...');
-            const oneWayElement = await page.evaluateHandle(() => {
-                return document.querySelector('#TripTypes_ow');
-            });
-            // @ts-ignore
-            await cursor.click(oneWayElement);
-
-            // Ensure button was selected, otherwise recurse
-            const isOneWaySelected: boolean = await page.evaluate(() => {
-                const oneWayRadioButton: HTMLInputElement = document.querySelector('#TripTypes_ow');
-                return oneWayRadioButton.checked;
-            });
-
-            if (!isOneWaySelected) {
-                this._logger.debug('Missed button, retrying...');
-                await clickOneWay();
-            }
-        };
-        await clickOneWay();
-
-        // Wait
-        await page.waitForTimeout(500);
-
-        // Type itinerary after clicking each input 3 times to select all and overwrite any existing values
-        this._logger.debug('Typing origin...');
-
-        await cursor.click('#Trips_0__Origin');
-        await page.type('#Trips_0__Origin', origin, { delay: 500 });
-
-        this._logger.debug('Typing destination...');
-        await cursor.click('#Trips_0__Destination');
-        await page.type('#Trips_0__Destination', destination, { delay: 500 });
-
-        this._logger.debug('Typing departure date...');
-        await cursor.click('#Trips_0__DepartDate');
-        await page.type('#Trips_0__DepartDate', date, { delay: 500 });
-
-        // Scroll to upgrade search filter dropdown
-        // TODO: Remove because unnecessary?
-        this._logger.debug('Scrolling...');
-        await page.evaluate(() => {
-            const dropdown = document.querySelector('#select-upgrade-type');
-            dropdown.scrollIntoView({
-                behavior: 'smooth',
-                block: 'end',
-                inline: 'end'
-            });
-        });
-
-        // Select upgrade search
-        this._logger.debug('Selecting upgrade type...');
-        await page.select('#select-upgrade-type', 'MUA');
-
-        // Fix focus
-        await cursor.click('#fare-preference');
-        await page.focus('#ClassofService');
-
-        // Wait
-        await page.waitForTimeout(1000);
-
-        // Search
-        this._logger.debug('Searching...');
-        const searchElement = await page.evaluateHandle(() => {
-            return document.querySelector('#btn-search');
-        });
-        // @ts-ignore
-        await cursor.click(searchElement);
-
-        // Wait for all results to load
-        await page.waitForNavigation();
-        await page.waitForNetworkIdle({ idleTime: 3000 });
-
-        // Stop ghost cursor
-        cursor.toggleRandomMove(false);
-        await page.waitForTimeout(2000);
-
-        // Close page
-        this._logger.debug('Closing page...');
-        await page.close();
-
-        return flights;
     }
 
     public getSpecificFlight(flightNumber: string, flights: IFlight[]): IFlight {
@@ -296,7 +161,7 @@ export class App {
 
             try {
                 // Get flights
-                const flights = await this.getFlights(
+                const flights = await this._flightManager.getFlights(
                     job.itinerary.origin,
                     job.itinerary.destination,
                     job.itinerary.departureDate
---
+++ src/helpers/flight-manager.ts
@@ -0,0 +1,165 @@
+import { createCursor, getRandomPagePoint, installMouseHelper } from 'ghost-cursor';
+
+import { WinstonLogger } from '../utils';
+import type { BaseLogger } from '../utils';
+import type { ChromeInstance } from '../helpers';
+import type { IFlightResponse, IFlight } from '../types/flights';
+
+interface IFlightManagerConfig {
+    chrome: ChromeInstance;
+}
+
+export class FlightManager {
+    private readonly _config: IFlightManagerConfig;
+    private readonly _logger: BaseLogger;
+
+    constructor(config: IFlightManagerConfig) {
+        // Set config
+        this._config = config;
+
+        // Configure class-wide logger
+        this._logger = new WinstonLogger('Flight Manager').logger;
+    }
+
+    public async getFlights(origin: string, destination: string, date: string): Promise<IFlight[]> {
+        const flights: IFlight[] = [];
+
+        // Open Chrome
+        this._logger.debug('Launching chrome...');
+        await this._config.chrome.launch();
+
+        // Launch page
+        this._logger.debug('Opening page...');
+        const page = await this._config.chrome.instance.newPage();
+        const cursor = createCursor(page, await getRandomPagePoint(page), true);
+        await installMouseHelper(page);
+
+        // Register listener to parse flights API response
+        page.on('response', async (response) => {
+            if (
+                response
+                    .url()
+                    .startsWith(
+                        'https://www.united.com/ual/en/us/flight-search/book-a-flight/flightshopping/getflightresults/rev'
+                    ) &&
+                response.status() === 200
+            ) {
+                // Parse response
+                const data = (await response.json()) as IFlightResponse;
+
+                // Check for API-native errors
+                if (data.status !== 'success') {
+                    this._logger.error(`Encountered errors: ${data.errors.join(', ')}`);
+                }
+
+                // Store remote flights
+                const remoteFlights = data.data.Trips[0]?.Flights;
+
+                // Ensure we got some flight results, and store them
+                if (!remoteFlights || !Array.isArray(remoteFlights)) {
+                    this._logger.warn('No flights found in batch');
+                } else {
+                    flights.push(...remoteFlights);
+                }
+
+                this._logger.debug('Flight batch acquired');
+            }
+        });
+
+        // Clear browser cookies
+        const client = await page.target().createCDPSession();
+        await client.send('Network.clearBrowserCookies');
+        await client.detach();
+
+        // Navigate to page
+        this._logger.debug('Navigating...');
+        await page.goto('https://www.united.com/ual/en/us/flight-search/book-a-flight', {
+            waitUntil: 'networkidle0'
+        });
+
+        // Click one way, and keep trying if cursor missed the button
+        const clickOneWay = async () => {
+            // Click one way
+            this._logger.debug('Clicking one way...');
+            const oneWayElement = await page.evaluateHandle(() => {
+                return document.querySelector('#TripTypes_ow');
+            });
+            // @ts-ignore
+            await cursor.click(oneWayElement);
+
+            // Ensure button was selected, otherwise recurse
+            const isOneWaySelected: boolean = await page.evaluate(() => {
+                const oneWayRadioButton: HTMLInputElement = document.querySelector('#TripTypes_ow');
+                return oneWayRadioButton.checked;
+            });
+
+            if (!isOneWaySelected) {
+                this._logger.debug('Missed button, retrying...');
+                await clickOneWay();
+            }
+        };
+        await clickOneWay();
+
+        // Wait
+        await page.waitForTimeout(500);
+
+        // Type itinerary after clicking each input 3 times to select all and overwrite any existing values
+        this._logger.debug('Typing origin...');
+
+        await cursor.click('#Trips_0__Origin');
+        await page.type('#Trips_0__Origin', origin, { delay: 500 });
+
+        this._logger.debug('Typing destination...');
+        await cursor.click('#Trips_0__Destination');
+        await page.type('#Trips_0__Destination', destination, { delay: 500 });
+
+        this._logger.debug('Typing departure date...');
+        await cursor.click('#Trips_0__DepartDate');
+        await page.type('#Trips_0__DepartDate', date, { delay: 500 });
+
+        // Scroll to upgrade search filter dropdown
+        // TODO: Remove because unnecessary?
+        this._logger.debug('Scrolling...');
+        await page.evaluate(() => {
+            const dropdown = document.querySelector('#select-upgrade-type');
+            dropdown.scrollIntoView({
+                behavior: 'smooth',
+                block: 'end',
+                inline: 'end'
+            });
+        });
+
+        // Select upgrade search
+        this._logger.debug('Selecting upgrade type...');
+        await page.select('#select-upgrade-type', 'MUA');
+
+        // Fix focus
+        await cursor.click('#fare-preference');
+        await page.focus('#ClassofService');
+
+        // Wait
+        await page.waitForTimeout(1000);
+
+        // Search
+        this._logger.debug('Searching...');
+        const searchElement = await page.evaluateHandle(() => {
+            return document.querySelector('#btn-search');
+        });
+        // @ts-ignore
+        await cursor.click(searchElement);
+
+        // Wait for all results to load
+        await page.waitForNavigation();
+        await page.waitForNetworkIdle({ idleTime: 3000 });
+
+        // Stop ghost cursor
+        cursor.toggleRandomMove(false);
+        await page.waitForTimeout(2000);
+
+        // Close page
+        this._logger.debug('Closing page...');
+        await page.close();
+
+        return flights;
+    }
+}
